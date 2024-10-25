d115b705392612eacddf99683c807d3b03c8baed
Affan Najiy <affannajiy@gmail.com>
2024-10-25 23:14:18 +0800

    Changed Folder Name

--- DS_W3Schools/DS_DataFrame.py
+++ DS_W3Schools/DS_DataFrame.py
@@ -1,4 +1,7 @@
 import pandas as pd
+#panda was not friendly to me
+#Reminder:
+#pip install pandas in command prompt, select interpreter for python in VS Code
 
 d = {'col1': [1, 2, 3, 4, 7], 'col2': [4, 5, 6, 9, 5], 'col3': [7, 8, 12, 1, 11]}
 
